7f0e231f620d6c6d4edffc7f8fbd04749c603cfd
Shehzeen <shehzeenalilive4others@gmail.com>
2024-04-05 15:13:23 +0500

    "Select one of the  following operators to perform action"

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const answer = await inquirer.prompt([
     { message: "Enter first number", type: "number", name: "firstNumber" },
     { message: "Enter second number", type: "number", name: "secondNumber" },
     {
-        message: "Select one of the operators to perform action",
+        message: "Select one of the  following operators to perform action",
         type: "list",
         name: "operator",
         choices: ["Addition", "Subtraction", "Multiplication", "Division"],
